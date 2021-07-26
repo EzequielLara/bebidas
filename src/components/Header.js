f06f123b324dbe9f84b0836400ef745a96a64a3c
Ezequiel <ezekdaw@gmail.com>
2021-07-26 20:14:20 +0200

    primer commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className = 'bg-alert'>
+                <h1>Busca Recetas de Bebidas</h1>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
