d41d55351464e2fb6d801d9ad6020f4999222b9c
Ezequiel <ezekdaw@gmail.com>
2021-07-29 00:23:36 +0200

    creando ventana modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,7 +1,88 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ModalContext } from '../context/ModalContext';
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { ModalConsumer } from '../context/ModalContext';
+
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+export const RecetaModal = () => {
+
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+    
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+    return ( 
+        <div>
+            <button type="button" onClick={handleOpen}>
+                Open Modal
+            </button>
+            <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={open}
+                onClose={handleClose}
+            >
+                <div style={modalStyle} className={classes.paper}>
+                <h2 id="simple-modal-title">Text in a modal</h2>
+                <p id="simple-modal-description">
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </p>
+                </div>
+            </Modal>
+        </div>
+     );
+}
+ 
+
+
+
 const Receta = ({receta}) => {
+
+    //Configuración del modal de materialUI
+    const [ modalStyle ] = useState(getModalStyle);
+    const [ open, setOpen] = useState(false);
+
+    const classes = useStyles();
+    const handleOpen = () =>{
+        setOpen(true);
+
+    }
+    const handleClose = () =>{
+        setOpen(false);
+    }
+
+    //Extraer los valores del context
 
     const { guardarIdReceta } = useContext(ModalContext);
 
@@ -17,9 +98,23 @@ const Receta = ({receta}) => {
                             className = 'btn btn-block btn-primary'
                             onClick = {(e) =>{
                                 e.preventDefault();
-                                guardarIdReceta(receta.idDrink)
+                                guardarIdReceta(receta.idDrink);
+                                handleOpen();
                             }}
                         >Ver Receta</button>
+
+                        <Modal
+                            open = {open}
+                            onClose = {()=>{
+                                guardarIdReceta(null);
+                                handleClose();
+
+                            }}
+                        >
+                            <div style = {modalStyle} className = {classes.paper}>
+                                <h1>Desde Modal</h1>
+                            </div>
+                        </Modal>
                     </div>
                 </div>
             </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {createContext, useState, useEffect} from 'react';
 
 
@@ -6,6 +7,27 @@ export const ModalContext = createContext();
 const ModalProvider =(props)=>{
 
     const [idreceta, guardarIdReceta] = useState(null);
+    const [info, guardarInfo] = useState({});
+
+    //una vez tenemos una receta, llamar a la API
+    useEffect(()=>{
+
+        const obtenerReceta = async () =>{
+
+            if(!idreceta) return;
+
+            const url =`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const informacion = await axios.get(url);
+
+            console.log(informacion.data.drinks[0]);
+
+            guardarInfo(informacion.data.drinks[0]);
+
+        }
+        obtenerReceta();
+
+    }, [idreceta])
 
     return (
         <ModalContext.Provider 
